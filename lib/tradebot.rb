9527ef561d8df2934723a93e6c96667713fc9dce
Amadeo Pellicce <pellicceama@gmail.com>
2015-01-28 22:30:34 +0000

    fixing specs

---
+++ lib/tradebot.rb
@@ -0,0 +1,10 @@
+require "tradebot/version"
+
+class Tradebot
+  def hit(pins)
+  end
+
+  def score
+    -1
+  end
+end
---
+++ spec/tradebot_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+class Tradebot
+  def hit(pins)
+  end
+
+  def score
+    0
+  end
+end
